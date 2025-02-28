59d05f25ec38caaac4199bf28a2ea305f6cda95d
Felipe César Porto Oliveira <136658021+felipe-cesar-dev@users.noreply.github.com>
2025-02-27 22:30:02 -0300

    Update index.jsx

--- app/index.jsx
+++ app/index.jsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <ActivityIndicator size={40} color={"#121212"}/>
+      <ActivityIndicator size={40} color={"green"}/>
     </View>
   );
 }
